252d5f8ce6be048890db3f25b81622bb182f4c52
mohammadnoghrei <noghreimohammad@gmail.com>
2024-05-29 00:03:09 +0330

    create customerentity

---
+++ src/main/java/madsilver/model/Customer.java
@@ -0,0 +1,24 @@
+package madsilver.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import madsilver.base.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Customer extends Person {
+    @OneToMany
+    private List<Order> orderList=new ArrayList<>();
+
+}
