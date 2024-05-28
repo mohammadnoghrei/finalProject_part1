e94e7bcae5031eab5fda6abc1500b674ea40d895
mohammadnoghrei <noghreimohammad@gmail.com>
2024-05-29 00:08:20 +0330

    create session factory singlton

---
+++ src/main/java/madsilver/connection/SessionFactorySingleton.java
@@ -0,0 +1,35 @@
+package madsilver.connection;
+
+
+
+import madsilver.model.Person;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+
+    private SessionFactorySingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+
+                    .addAnnotatedClass(Person.class)
+
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
