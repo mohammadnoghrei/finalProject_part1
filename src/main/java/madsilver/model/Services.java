405fa6f6c3a3e3a8b59dd98ff0f152ee185cc0be
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-03 00:38:16 +0330

    fix Services.java

--- src/main/java/madsilver/model/Services.java
+++ src/main/java/madsilver/model/Services.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import madsilver.base.entity.BaseEntity;
 
 import java.util.ArrayList;
@@ -13,14 +14,14 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Services extends BaseEntity<Long> {
     @Column(nullable = false,unique = true)
     private String ServiceName;
-    @OneToMany(mappedBy = "Services",cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    @OneToMany(mappedBy = "services",cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
     private List<SubServices> subServicesList=new ArrayList<>();
 
 }
