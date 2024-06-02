f7c5bdb94fdd6dfc54537e46fb86f2d0a9dfce62
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-03 00:37:26 +0330

    fix Order.java

--- src/main/java/madsilver/model/Order.java
+++ src/main/java/madsilver/model/Order.java
@@ -2,31 +2,41 @@ package madsilver.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import madsilver.base.entity.BaseEntity;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Table(name = "orderOfCustomer")
+@Table(name = "orders")
 public class Order extends BaseEntity<Long> {
-  @ManyToOne
+    @ManyToOne
     private Customer customer;
-  @ManyToOne
+    @ManyToOne
     private Expert expert;
-    private double orderPrice;
+    @ManyToOne
+    private SubServices subServices;
+    private double customerOfferPrice;
+    private double ExpertOfferPrice;
+    private double finalPrice;
     private OrderStatus orderStatus;
     private LocalDate orderRegisterDate;
     private LocalDate RequestedDateToDoOrder;
     private LocalDate ToDoOrderDate;
     private boolean doOrder;
+    @Range(min = 0, max = 5)
+    private int rate;
+    private String description;
     @OneToOne
     private Comment comment;
+    private String address;
 
 
 
