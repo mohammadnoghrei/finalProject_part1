1bcc8fad5c088c812050cc5933ca2308936dd94a
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-03 00:35:29 +0330

    fix base package

--- src/main/java/madsilver/base/entity/BaseEntity.java
+++ src/main/java/madsilver/base/entity/BaseEntity.java
@@ -8,11 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 @Getter
 @Setter
 @AllArgsConstructor
+@SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity <ID extends Serializable> implements Serializable{
