25be4ae1ebf7f5f728bd1fd433216cfc773890e8
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-03 00:37:55 +0330

    create SecurityContext.java

---
+++ src/main/java/madsilver/utility/SecurityContext.java
@@ -0,0 +1,27 @@
+package madsilver.utility;
+
+
+
+
+import madsilver.model.Admin;
+import madsilver.model.Customer;
+import madsilver.model.Expert;
+
+public class SecurityContext {
+
+    public static long id;
+    public static String firstName;
+    public static String lastName;
+    public static String nationalCode;
+    public static String password;
+    public static String phoneNumber;
+    public static Customer customer;
+    public static Expert expert;
+
+    public static Admin admin;
+
+
+
+
+
+}
