72f7c705e7855474798b037def5757da09d313f2
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-03 00:37:14 +0330

    fix Offer.java

--- src/main/java/madsilver/model/Offer.java
+++ src/main/java/madsilver/model/Offer.java
@@ -1,6 +1,16 @@
 package madsilver.model;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import madsilver.base.entity.BaseEntity;
-
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+//@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Offer extends BaseEntity<Long> {
+
 }
