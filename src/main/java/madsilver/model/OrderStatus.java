30fb853022ede29af62d934bcb4603a898908b44
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-05 00:38:48 +0330

    create order status

--- src/main/java/madsilver/model/OrderStatus.java
+++ src/main/java/madsilver/model/OrderStatus.java
@@ -1,4 +1,10 @@
 package madsilver.model;
 
 public enum OrderStatus {
+     WAITING_FOR_EXPERT_OFFER,
+    WAITING_FOR_CHOOSE_EXPERT,
+    WAITING_FOR_COMING_EXPERT_TO_YOUR_LOCATION,
+    START_SERVICE,
+    DONE_SERVICE,
+    PAID_SERVICE_PRICE
 }
