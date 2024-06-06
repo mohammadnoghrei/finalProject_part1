a1a7fcdf31c9f30b4fa2e30eedeb94250860debe
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-06 20:08:27 +0330

    cleaning the code

--- src/main/java/madsilver/repository/CommentRepository.java
+++ src/main/java/madsilver/repository/CommentRepository.java
@@ -1,7 +1,7 @@
 package madsilver.repository;
 
 import madsilver.base.repository.BaseRepository;
-import madsilver.model.Admin;
+
 import madsilver.model.Comment;
 
 public interface CommentRepository extends BaseRepository<Comment, Long> {
